52311ff2445d57eab729a6dfb5f9dbc73602504a
Wendy Sanarwanto <saintc0d3r@gmail.com>
2012-10-22 01:08:16 +0800

    Commit some left files.

---
+++ Sample.Extjs.Play.Mvc.SimpleCrud/app/controllers/Application.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+import com.typesafe.config.ConfigFactory;
+
+public class Application extends Controller {
+  
+  public static Result index() {
+	  return ok(index.render(ConfigFactory.load().getString("Index.Title")));
+  }
+  
+}
