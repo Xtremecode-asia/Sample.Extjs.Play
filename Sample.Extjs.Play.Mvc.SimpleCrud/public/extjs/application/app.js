2fb1acb505414632b0b77815f9cf4ff1df5a5c62
unknown <ASUS@JUGGERNAUT.(none)>
2012-10-24 06:52:38 +0800

    Add dynamic child node creation method for Employee

--- Sample.Extjs.Play.Mvc.SimpleCrud/public/extjs/application/app.js
+++ Sample.Extjs.Play.Mvc.SimpleCrud/public/extjs/application/app.js
@@ -1,24 +1,114 @@
+// -- Entry Point
 Ext.application({
 		name: 'Sample.ExtJs.Play.Mvc.SimpleCrud',
-		launch: function() {
-			var mainViewPort = 
-				Ext.create('Ext.container.Viewport', {
-						layout: 'border',
-						items: [{
-							region: 'north',
-							title: '.:: Sample MVC CRUD ::.',
-							margins: '3 3 3 3',
-							items: [{
-								xtype: 'toolbar',
-								items: [{
-									xtype: 'button',
-									text: 'File'
-								}, {
-									xtype: 'button',
-									text: 'Help'
-								}]
-							}]
-//							html: '<h1>TODO -- Put main menu & Toolbar here</h1>'
+		launch: onApplicationLaunched
+	}
+);
+
+// Helpers
+function onApplicationLaunched() {
+	var mainViewPort = 
+		Ext.create('Ext.container.Viewport', {				
+				layout: 'border',
+				items: [{
+					region: 'north',
+					title: '.:: Sample MVC CRUD ::.',
+					margins: '3 3 3 3'					
+				}, {
+					region: 'west',
+					id: 'panelWest',
+					collapsible: true,
+					title: 'Functions',							
+					width: 190,
+					margins: '0 2 0 3',					
+					layout: 'fit',
+					items: buildTreePanel()
+				}, {
+					region: 'south',
+					collapsible: false,
+					split: false,
+					html: '<h2> TODO -- Add status bar here </h2> ',
+					height: 20,
+					minHeight: 20
+				}, {
+					region: 'center',
+					items: [builToolbar(),	
+					{	
+						xtype: 'tabpanel',
+						id: 'mainTabPanel',
+						html: '<h3>Double click the function item to display its tab.</h3>'
+//						activeTab: 0,
+//						items: {
+//							title: 'Default Tab',
+//							closable: true,
+//            				html: 'The first tab\'s content. Others may be added dynamically'
+//						}
+					}]
+				}]
+			}
+		);			
+}
+
+function buildTreePanel(){
+	return [{	xtype: 'treepanel',
+				root: { 
+					text: 'Root', 
+					expanded: true,
+					id: 'rootNode',
+					children: [{
+						text: 'Master Data',
+						expanded: true,
+						id: 'masterDataNode',
+						children: buildMasterDataNodes()
+					}, {
+						text: 'Transactional Data',
+						expanded: true,
+						id: 'transactionalDataNode'
+					}, {
+						text: 'Reports',
+						expanded: true,
+						id: 'reportsNode'
+					}]									
+				},
+				listeners: {
+					itemdblclick: function (parent, record, item, index, e, eOpts ){
+						onMainTreePanelDoubleClicked(parent, record, item, index, e, eOpts);										
+					}
+				}
+			}];
+}
+
+function onMainTreePanelDoubleClicked(parent, record, item, index, e, eOpts ){
+	// Does the user click a leaf node in the main tree view ?
+	if (record.isLeaf() && record.raw.fields.length > 1){
+ 		var nodeTabId = record.raw.fields[1].tabId;
+ 		var nodeTab = Ext.getCmp(nodeTabId);
+ 		// Try to get the active panel 1st
+ 		var mainTabPanel = Ext.getCmp('mainTabPanel');
+		if (nodeTab == null){			
+			// The requested panel does not exist. So let's create it 1st
+			nodeTab = mainTabPanel.add( record.raw.fields[0].tabConfigs );			
+		}
+		mainTabPanel.setActiveTab(nodeTab);		
+	}
+}
+
+function buildMasterDataNodes(){
+	// TODO -- Add other nodes
+	return [ buildChildNodeConfig('Employee', 'employeeNode','employeeTab') ];
+}
+
+function buildChildNodeConfig( tabTitle, nodeId, tabId){
+	// TODO -- Changes the parameters to be in an array of params
+	return { text: tabTitle,
+			  id: nodeId,
+			  leaf: true,
+			  fields: [{ tabConfigs: { title: tabTitle, closable: true, id: tabId } },
+			  		   { tabId: tabId}]
+	};
+}
+
+function builToolbar(){
 //							items: [{
 //								xtype: 'menu',
 //								floatable: true,
@@ -29,58 +119,15 @@ Ext.application({
 //									text: 'help'
 //								}]													
 //							}]
-							
-						}, {
-							region: 'west',
-							id: 'panelWest',
-							collapsible: true,
-							title: 'Functions',							
-							width: 190,
-							layout: 'fit',
-							items: [{
-								xtype: 'treepanel',
-								root: { 
-										text: 'Root', 
-										expanded: true,
-										id: 'rootNode',
-										children: [{
-											text: 'Master Data',
-											expanded: true,
-											id: 'masterDataNode'
-										}, {
-											text: 'Transactional Data',
-											expanded: true,
-											id: 'transactionalDataNode'
-										}, {
-											text: 'Reports',
-											expanded: true,
-											id: 'reportsNode'
-										}]									
-									}
-							}]
-						}, {
-							region: 'south',
-							collapsible: false,
-							split: false,
-							html: '<h2> TODO -- Add status bar here </h2> ',
-							height: 20,
-							minHeight: 20
-						},{
-							region: 'center',
-							xtype: 'tabpanel',
-							html: '<h3>Double click the function item to display its tab.</h3>'
-	//						activeTab: 0,
-	//						items: {
-	//							title: 'Default Tab',
-	//							closable: true,
-	//            				html: 'The first tab\'s content. Others may be added dynamically'
-	//						}
-						}]
-					}
-				);			
-//			var panelWest = mainViewPort.getLayout().getLayoutItems()[1];
-//			console.info(panelWest.items);
-//			var panelWest = currentLayout.westRegion;
-		}
-	}
-);
+					
+	
+	return 	{	xtype: 'toolbar',
+				items: [{
+					xtype: 'button',
+					text: 'File' 
+				}, {
+					xtype: 'button',
+					text: 'Help'
+				}]
+			};	
+}
